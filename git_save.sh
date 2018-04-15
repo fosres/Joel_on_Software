ddc1eaac761ef30c0b06180179b80b8fbd2850c7
Tanveer Salim <tanveerasalim@gmail.com>
2018-04-14 22:06:30 -0700

    Joel_on_Software

---
+++ git_save.sh
@@ -0,0 +1,19 @@
+# git_save.sh
+# Saves git local repository for Joel_on_Software
+# to GitHub
+
+# To run git_save.sh:
+# Type "bash git_save.sh"
+# in BASH and press ENTER.
+
+#!/bin/bash
+
+	#Download everything from GitHub to local repostory
+
+	git pull
+	
+	git add .
+
+	git commit -m Joel_on_Software
+
+	git push origin Joel_on_Software
